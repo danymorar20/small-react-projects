adc3032ee639d9bc340e33760b722af7ed44610d
danymorar20 <danymorar20@gmail.com>
2024-11-24 17:22:54 -0600

    mouse follower project added

---
+++ projects/mouse-follower/src/App.jsx
@@ -0,0 +1,12 @@
+import { FollowMouse } from "./components/FollowMouse"
+
+function App() {
+  return (
+    <main>
+      <h3>Mouse follower</h3>
+      <FollowMouse/>
+    </main>
+  )
+}
+
+export default App
